99c43c8218e7a5ce959096e1e5cd9eded344005f
llev <llev1@me.com>
2021-10-11 15:27:57 +0100

    Created toggle from first two Level up videos

---
+++ src/lib/toggle.svelte
@@ -0,0 +1,56 @@
+<script>
+    export let isToggled = false
+    export let label = ""
+    export let style = ""
+</script>
+
+<label {style}>
+    <input type="checkbox" bind:checked={isToggled} />
+    <div class="toggle" />
+    {label}
+</label>
+
+
+
+<style>
+    label {
+        --width: 40px;
+        --height: calc(var(--width) / 2);
+        --radius: calc(var(--height) / 2);
+        display: flex;
+    }
+    .toggle {
+        position: relative;
+        width: var(--width);
+        height: var(--height);
+        border-radius: var(--radius);
+        background-color: var(--toggleBackgroundColour, lightGrey);
+        transition: background-colour 0.3s ease;
+        margin-right: 5px;
+    }
+    .toggle::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: var(--height);
+        width: var(--height);
+        border-radius: var(--radius);
+        background-color: var(--toggleButtonColor, white);
+        box-shadow: 1px 1px 5px rgba(0,0,0, 0.3);
+        transition: transform 0.3s ease;
+    }
+    input {
+        display: none;
+    }
+
+    input:checked + .toggle {
+        background-color: var(--toggleCheckedBackgroundColour, yellowgreen);
+    }
+
+    input:checked + .toggle::after {
+        transform: translate3d(100%, 0, 0);
+    }
+
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
+<script>
+    import Toggle from "$lib/toggle.svelte"
+    let isToggled = false
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Toggle bind:isToggled label="Test"/>
+<Toggle bind:isToggled label="Test" style="--toggleBackgroundColour: red;"/>
+
+{#if isToggled}
+    <h1>I'm toggled</h1>
+{/if}
