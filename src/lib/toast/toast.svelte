eb5b96010ff8b033c31bfac5fd75f3b66fe1d7c9
llev <llev1@me.com>
2021-10-14 09:35:16 +0100

    Added the initial toast files

---
+++ src/lib/toast/toast.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Portal from '../Portal.svelte'
+    import { toast } from './toast'
+</script>
+
+<Portal>
+    <div class="toast-wrapper">
+        {#each $toast as message}
+            <div class="toast-message">
+                <p>
+                    {message}
+                </p>
+            </div>
+        {/each}
+    </div>
+</Portal>
+
+<style>
+    .toast-wrapper {
+        
+    }
+</style>
